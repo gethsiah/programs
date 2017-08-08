b6a68674c103f1b06821f72813ab2846d3686515
gethsiah <30549406+gethsiah@users.noreply.github.com>
2017-08-08 16:32:10 +0530

    Create lakshmi.c

---
+++ lakshmi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int v;
+printf("Enter num");
+scanf("%d",&v);
+if(v%2==0)
+{
+printf("NUm is Even:);
+}
+else
+{
+printf("Num is odd");
+}
+getch();
+}
